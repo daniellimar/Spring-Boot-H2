d23129e9f22aa91917dab88ce3fa9692eb8de3f8
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2024-06-18 22:51:58 -0400

    first commit

---
+++ src/main/java/io/github/lucasAlencar/vendasApi.java
@@ -0,0 +1,18 @@
+package io.github.lucasAlencar;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class vendasApi {
+    public static void main(String[] args) {
+        SpringApplication.run(vendasApi.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner initialization() {
+        return args -> System.out.println("Hello and welcome!xx");
+    }
+}
